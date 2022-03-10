72d6bc649da3b0fc2a0fa8b91336243cefd2ea91
Arlan Gustavo Biati <arlan@MacBook-Air-de-Arlan.local>
2022-03-10 06:55:07 -0300

    :sparkles: implementação das funções de agendamento

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { StatusBar, Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
+import { format } from 'date-fns';
 
 import { BackButton } from '../../components/BackButton';
 import { Button } from '../../components/Button';
-import { Calendar } from '../../components/Calendar';
+import { Calendar, DayProps, genereteInterval, MarketDateProps } from '../../components/Calendar';
+
+import { CarDTO } from '../../dtos/CarDTO';
+import { getPlatformDate } from '../../utils/getPlatformDate';
 
 import ArrowSvg from '../../assets/arrow.svg'
 
@@ -21,16 +25,72 @@ import {
   Footer
 } from './styles';
 
+interface NavigationProps{
+  navigate:(
+    screen: string,
+    carObject:{
+      car: CarDTO,
+      dates: string[]
+    }
+  ) => void,
+  goBack: () => void
+}
+interface RentalPeriodProps {
+  startFormatted: string;
+  endFormatted: string;
+}
+
+interface ParamsProps {
+  car: CarDTO
+}
+
 export function Scheduling(){
+  const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>({} as DayProps);
+  const [marketDates, setMaketDates] = useState<MarketDateProps>({} as MarketDateProps);
+  const [rentalPeriod, setRentalPeriod] = useState<RentalPeriodProps>({} as RentalPeriodProps);
+
   const theme = useTheme();
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationProps>();
+  const route = useRoute();
+
+  const { car } = route.params as ParamsProps;
 
   function handleConfirmRental() {
-    navigation.navigate('SchedulingDetails' as never);
+    if(!rentalPeriod.startFormatted || !rentalPeriod.endFormatted) {
+      Alert.alert('Selecione o intervalo para alugar.')
+    } else {
+      navigation.navigate('SchedulingDetails', {
+        car,
+        dates: Object.keys(marketDates)
+      });
+    }
   }
 
   function handleBack() {
     navigation.goBack();
+  }
+
+  function handleChangeDate(date: DayProps) {
+    let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+    let end = date;
+
+    if(start.timestamp > end.timestamp) {
+      start = end;
+      end = start;
+    }
+
+    setLastSelectedDate(end);
+
+    const interval = genereteInterval(start, end);
+    setMaketDates(interval);
+
+    const firstDate = Object.keys(interval)[0];
+    const endDate = Object.keys(interval)[Object.keys(interval).length - 1];
+
+    setRentalPeriod({
+      startFormatted: format(getPlatformDate(new Date(firstDate)), 'dd/MM/yyyy'),
+      endFormatted: format(getPlatformDate(new Date(endDate)),  'dd/MM/yyyy')
+    })
   }
 
   return (
@@ -56,21 +116,28 @@ export function Scheduling(){
 
           <DateInfo>
             <DateTitle>DE</DateTitle>
-            <DateValue selected={false} />
+            <DateValue selected={!!rentalPeriod.startFormatted}>
+              {rentalPeriod.startFormatted}
+            </DateValue>
           </DateInfo>
 
           <ArrowSvg />
 
           <DateInfo>
             <DateTitle>ATÉ</DateTitle>
-            <DateValue selected={false} />
+            <DateValue selected={!!rentalPeriod.endFormatted}>
+              {rentalPeriod.endFormatted}
+            </DateValue>
           </DateInfo>
 
         </RentalPeriod>
       </Header>
 
       <Content>
-        <Calendar />
+        <Calendar
+          markedDates={marketDates}
+          onDayPress={handleChangeDate}
+        />
       </Content>
 
       <Footer>
