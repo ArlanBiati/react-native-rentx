58e5f211fd80e1da40309aab26249caa91e3645c
--global <--global>
2022-04-26 22:20:50 -0300

    :sparkles: implementação da validação dos campos de criação de usuário

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,6 +1,13 @@
+import React, { useState } from 'react';
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback
+} from 'react-native';
+import * as Yup from 'yup';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
+
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
 import { Button } from '../../../components/Button';
@@ -16,11 +23,50 @@ import {
   FormTitle
 } from './styles';
 
-export function SignUpFirstStep(){
-  const navigation = useNavigation();
+interface NavigationProps{
+  navigate:(
+    screen: string,
+    userObject:{
+      user: {
+        name: string;
+        email: string;
+        driverLicense: string;
+      }
+    }
+  ) => void,
+  goBack: () => void
+}
 
-  function handleNextStep() {
-    navigation.navigate('SignUpSecondStep' as never)
+export function SignUpFirstStep(){
+  const navigation = useNavigation<NavigationProps>();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [driverLicense, setDriverLincense] = useState('');
+
+  async function handleNextStep() {
+    try {
+      const schema = Yup.object({
+        driverLicense: Yup.string()
+          .required('CNH é obrigatória'),
+
+        email: Yup.string()
+          .email('E-mail inválido')
+          .required('Email é obrigatório'),
+
+        name: Yup.string()
+        .required('Nome é obrigatório')
+      })
+
+      const data = { name, email, driverLicense };
+      await schema .validate(data);
+
+      navigation.navigate('SignUpSecondStep' as never, { user: data });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', error.message);
+      }
+    }
   }
 
   function handleBack() {
@@ -53,18 +99,25 @@ export function SignUpFirstStep(){
             <Input
               iconName='user'
               placeholder='Nome'
+              onChangeText={setName}
+              value={name}
             />
 
             <Input
               iconName='mail'
               placeholder='E-mail'
               keyboardType='email-address'
+              onChangeText={setEmail}
+              value={email}
+              autoCapitalize='none'
             />
 
             <Input
               iconName='credit-card'
               placeholder='CNH'
               keyboardType='numeric'
+              onChangeText={setDriverLincense}
+              value={driverLicense}
             />
           </Form>
 
