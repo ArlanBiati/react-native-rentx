994a97c74ef4227923d9951594e9c61b41d88649
--global <--global>
2022-08-08 13:25:26 -0300

    :sparkles: implementando interface de perfil

---
+++ src/screens/Profile/styles.ts
@@ -0,0 +1,111 @@
+import styled, { css } from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface OptionProps {
+  active: boolean;
+}
+
+export const Container = styled.View`
+  flex: 1;
+
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 227px;
+
+  padding: 0 24px;
+
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.header};
+`;
+
+export const HeaderTop = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: ${getStatusBarHeight() + 32}px;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: ${RFValue(25)}px;
+  font-family: ${({ theme }) => theme.fonts.medium_archivo};
+
+  color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+export const LogoutButton = styled.TouchableOpacity``;
+
+export const PhotoContainer = styled.View`
+  width: 180px;
+  height: 180px;
+
+  border-radius: 90px;
+
+  background-color: ${({ theme }) => theme.colors.shape};
+
+  margin-top: 48px;
+`;
+
+export const Photo = styled.Image`
+  width: 180px;
+  height: 180px;
+
+  border-radius: 90px;
+`;
+
+export const PhotoButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+
+  border-radius: 20px;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.main};
+
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+
+  padding: 0 24px;
+  margin-top: 122px;
+`;
+
+export const Options = styled.View`
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.colors.line};
+
+  flex-direction: row;
+  justify-content: space-around;
+
+  margin-bottom: 24px;
+`;
+
+export const Option = styled.TouchableOpacity<OptionProps>`
+    padding-bottom: 14px;
+
+  ${({ active }) => active && css`
+    border-bottom-width: 2px;
+    border-bottom-color: ${({ theme }) => theme.colors.main};
+  `}
+`;
+
+export const OptionTitle = styled.Text<OptionProps>`
+  font-size: ${RFValue(20)}px;
+  font-family: ${({ theme, active }) => active ? theme.fonts.bold_archivo : theme.fonts.medium_archivo};
+
+  color: ${({ theme, active }) => active ? theme.colors.header : theme.colors.text_detail};
+`;
+
